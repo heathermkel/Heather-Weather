383250473389ba38e30f707c337106d0ea7de75f
heathermkel <79683310+heathermkel@users.noreply.github.com>
2021-07-17 09:43:55 -0500

    Added a footer

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ function App() {
   <h1>Hello from React</h1>
   <Weather />
   </header>
+
+   <footer> 
+      This page was built by 
+      <a href="https://www.linkedin.com/in/heatherk3lly/" target="_blank"> Heather Kelly. </a>
+      
+      Open source code:
+      <a href="https://github.com/heathermkel/Heather-Weather" target="_blank">  Git Hub</a>
+     
+      </footer>
   </div>
   );
 }
